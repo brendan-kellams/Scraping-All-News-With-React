0a937a45cc1b0cbd71d13e7e4175ffd57718ecc6
Brendan Kellams <brendan.kellams@gmail.com>
2018-01-26 21:33:17 -0800

    adding more required folders

--- scraping-with-react/app.js
+++ scraping-with-react/app.js
@@ -5,9 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -26,8 +23,9 @@ app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
+const index = require('./routes/index');
 app.use('/', index);
-app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
