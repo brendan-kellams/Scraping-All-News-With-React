7aa79452b2fef293a55ac0486ea7a632d2449ca4
Brendan Kellams <brendan.kellams@gmail.com>
2018-01-30 17:38:19 -0800

    creating files and adding some React

---
+++ scraping-with-react/client/src/components/Buttons/DeleteBtn.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export const DeleteBtn = props =>
+    <span className= "delete-btn" {...props}>
+        Delete
+    </span>;
---
+++ scraping-with-react/client/src/components/Buttons/SaveBtn.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export const SaveBtn = props =>
+    <span className= "save-btn" {...props}>
+        Save
+    </span>;
